8b96908e6187141708ff30af5490feed09f7b73c
matt <matthis.chicault@gmail.com>
2024-08-14 18:25:18 +0200

    add clientReady.ts

---
+++ src/discord/events/clientReady.ts
@@ -0,0 +1,39 @@
+import { CronJob } from 'cron';
+import { Client, Events } from 'discord.js';
+import { BotEvent } from '../../types';
+import { deleteDirFiles } from '../../utils/helpers';
+import { deleteDbOldMessages } from '../../utils/messages/message-db';
+import logger from '../../utils/pino-logger';
+
+const eventName = 'clientReady';
+
+const event: BotEvent = {
+    name: Events.ClientReady,
+    once: true,
+
+    execute: async (client: Client) => {
+        if (!client.user || !client.application) return;
+
+        CronJob.from({
+            cronTime: '0 * * * *', // every hour
+            onTick: () => { deleteDirFiles('tmp') },
+            start: true
+        });
+        CronJob.from({
+            cronTime: '0 * * * *', // every hour
+            onTick: () => { deleteDbOldMessages() },
+            start: true
+        });
+
+        return logger.info({
+            app: 'Bot',
+            event: eventName,
+            clientId: client.user.id,
+            clientUser: client.user.tag,
+            commands: client.commands.size,
+            events: client.events.size
+        }, `Logged in as ${client.user.tag}`);
+    }
+};
+
+export default event;
