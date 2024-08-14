6a1700d37ba42df1f07baceeb936c6af09555dcf
matt <matthis.chicault@gmail.com>
2024-08-14 18:24:43 +0200

    add interactionCreate.ts

---
+++ src/discord/events/interactionCreate.ts
@@ -0,0 +1,85 @@
+import { Events, Interaction } from 'discord.js';
+import short from 'short-uuid';
+import { BotEvent } from '../../types';
+import { errorEmbed } from '../../utils/helpers';
+import logger from '../../utils/pino-logger';
+
+const eventName = 'interactionCreate';
+
+const event: BotEvent = {
+    name: Events.InteractionCreate,
+
+    execute: async (interaction: Interaction) => {
+        if (!interaction.inCachedGuild()) return;
+        const suuid = short();
+        const uuid = suuid.new();
+
+        if (interaction.isChatInputCommand()) {
+            let command = interaction.client.commands.get(interaction.commandName);
+            let cooldown = interaction.client.cooldowns.get(`${interaction.commandName}-${interaction.user.username}`);
+            if (!command) return;
+
+            if (command.cooldown && cooldown) {
+                if (Date.now() < cooldown) {
+                    const waiting = Math.floor(Math.abs(Date.now() - cooldown) / 1000);
+                    errorEmbed(interaction, `You have to wait ${waiting} second${waiting > 1 ? 's' : ''} to use this command again`);
+                    setTimeout(() => interaction.deleteReply(), 5000);
+                    return;
+                }
+                interaction.client.cooldowns.set(`${interaction.commandName}-${interaction.user.username}`, Date.now() + command.cooldown * 1000);
+                setTimeout(() => {
+                    interaction.client.cooldowns.delete(`${interaction.commandName}-${interaction.user.username}`)
+                }, command.cooldown * 1000);
+            } else if (command.cooldown && !cooldown) {
+                interaction.client.cooldowns.set(`${interaction.commandName}-${interaction.user.username}`, Date.now() + command.cooldown * 1000);
+            }
+
+            try {
+                command.execute(interaction, uuid);
+            } catch (err) {
+                logger.error({
+                    app: 'Bot',
+                    event: eventName,
+                    action: 'execute',
+                    command: command,
+                    uuid: uuid,
+                    err: err
+                }, `Error while executing ${command} command`);
+            }
+            //console.timeEnd();
+            return;
+        }
+
+        else if (interaction.isAutocomplete()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) {
+                logger.error({
+                    app: 'Bot',
+                    action: 'autocomplete',
+                    event: eventName,
+                    command: interaction.commandName,
+                    uuid: uuid
+                }, `No command matching ${interaction.commandName} was found`);
+                return;
+            }
+
+            try {
+                if (!command.autocomplete) return;
+                command.autocomplete(interaction);
+            } catch (err) {
+                logger.error({
+                    app: 'Bot',
+                    action: 'autocomplete',
+                    event: eventName,
+                    command: command,
+                    uuid: uuid,
+                    err: err
+                }, `Error while autocompleting ${command} command`);
+            }
+            return;
+        }
+    }
+}
+
+export default event;
