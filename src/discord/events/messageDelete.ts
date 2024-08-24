40fc9a7df22eb4ef559eef446b9238f902df2329
matt <matthis.chicault@gmail.com>
2024-08-24 02:57:22 +0200

    add partial messages

--- src/discord/events/messageDelete.ts
+++ src/discord/events/messageDelete.ts
@@ -2,7 +2,7 @@ import { AuditLogEvent, ChannelType, Events, Message, PartialMessage } from 'dis
 import short from 'short-uuid';
 import { BotEvent, WebhookEvent } from '../../types';
 import { MAX_EMBED_FIELD_VALUE } from '../../utils/constants';
-import { chunkify, getMember } from '../../utils/helpers';
+import { chunkify, getMember, getUser } from '../../utils/helpers';
 import { getCacheMessage } from '../../utils/messages/message-cache';
 import { getDbMessage } from '../../utils/messages/message-db';
 import { webhookSend } from '../../utils/webhooks';
@@ -13,22 +13,22 @@ const event: BotEvent = {
     name: Events.MessageDelete,
 
     execute: async (message: Message | PartialMessage) => {
-        if (!message.author || message.author.bot || !message.guild || message.channel.type === ChannelType.DM) return;
+        if (message.author?.bot || !message.guild || message.channel.type === ChannelType.DM) return;
 
-        const user = message.author;
-        if (!user) return;
-
-        const logs = await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete, limit: 5 }).catch(err => { });
-        const log = logs?.entries.find(e => e.targetId === user.id && new Date().getTime() - e.createdTimestamp < 3000);
-
-        const suuid = short();
-        const uuid = suuid.new();
-        const member = await getMember(message.guild, user?.id);
-        const executor = log?.executor ?? user;
-
+        let userId = message.author?.id;
         let cachedMessage = getCacheMessage(message.id);
         if (!cachedMessage) cachedMessage = await getDbMessage(message.id);
         if (!cachedMessage) return;
+        if (!userId) userId = cachedMessage.authorId;
+
+        const logs = await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete, limit: 5 }).catch(err => { });
+        const log = logs?.entries.find(e => e.targetId === userId && new Date().getTime() - e.createdTimestamp < 3000);
+
+        const suuid = short();
+        const uuid = suuid.new();
+        const user = await getUser(message.guild, userId);
+        const member = await getMember(message.guild, user?.id);
+        const executor = log?.executor ?? user;
 
         const messageDeleteEvent: WebhookEvent = {
             id: uuid,
@@ -37,8 +37,8 @@ const event: BotEvent = {
             timestamp: new Date(),
             embeds: [{
                 author: {
-                    name: `${user.tag ?? 'Unknown user'} ${member && member.nickname ? `(${member.nickname})` : ''}`,
-                    iconURL: user.avatarURL() ?? Bun.env.USER_DEFAULT_AVATAR!
+                    name: `${user?.tag ?? 'Unknown user'} ${member && member.nickname ? `(${member.nickname})` : ''}`,
+                    iconURL: user?.avatarURL() ?? Bun.env.USER_DEFAULT_AVATAR!
                 },
                 description: `Message deleted in ${message.channel} (#${message.channel.name})`,
                 fields: [
@@ -66,7 +66,7 @@ const event: BotEvent = {
         });
 
         messageDeleteEvent.embeds[0].fields.push(
-            { name: 'ID', value: `\`\`\`ini\n${user !== executor ? `Executor=${executor?.id ?? '???'}\n` : ''}Author=${message.author.id}\nMessage=${cachedMessage.id}\nChannel=${message.channel.id}\`\`\`` }
+            { name: 'ID', value: `\`\`\`ini\n${user !== executor ? `Executor=${executor?.id ?? '???'}\n` : ''}Author=${user?.id ?? '???'}\nMessage=${cachedMessage.id}\nChannel=${message.channel.id}\`\`\`` }
         );
 
         await webhookSend(messageDeleteEvent);
