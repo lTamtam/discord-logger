876422da2af18d3de8c90f95591e50065baaeee7
matt <matthis.chicault@gmail.com>
2024-09-01 22:48:21 +0200

    add hex colors for roles

--- src/discord/events/guildRoleCreate.ts
+++ src/discord/events/guildRoleCreate.ts
@@ -33,6 +33,7 @@ const event: BotEvent = {
                 description: `${auto ? 'Managed role' : 'Role'} ${role} was created`,
                 fields: [
                     { name: 'Name', value: role.name },
+                    { name: 'Color', value: role.hexColor },
                     { name: 'Permissions', value: permissions ? permissions : '\`<None>\`' }
                 ],
                 footer: { text: `ID: ${uuid}` },
